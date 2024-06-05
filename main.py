1ac3533ade8b30127a91b7e9022e0d089dddd645
serg_myst <serg_myst@mail.ru>
2024-06-05 12:19:20 +0300

    cors for frontend, new endpoint

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from operations.router import router_department as router_departments
 from operations.router import router_user as router_users
 from operations.router import router_task as router_task
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='BT-24 API')
+
+origins = [
+    'http://localhost',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/', include_in_schema=False)
--- services/get_tasks_async.py
+++ services/get_tasks_async.py
@@ -1,6 +1,10 @@
 from config import URL
 from schemas.schemas import Task
 from pydantic import ValidationError
+from .get_params import Params
+import aiohttp
+import asyncio
+from pprint import pprint
 
 
 def get_content(request: dict) -> list:
@@ -22,6 +26,80 @@ async def get_tasks_by_parameters(session, params: dict):
             return tasks_list
         else:
             print(f'Ошибка получения данных методом {method}. Статус {response.status_code}')
+
+
+async def get_user_tasks_by_parameters(session, params: dict, user):
+    method = 'tasks.task.list'
+    tasks_list = []
+    async with session.request(method='get', url=f'{URL}/{method}', params=params) as response:
+        if response.status == 200:
+            content = await response.json()
+            tasks = get_content(content)
+            tasks_list = get_task_list(tasks, tasks_list)
+            user.tasks.append(tasks_list)
+        else:
+            print(f'Ошибка получения данных методом {method}. Статус {response.status_code}')
+
+
+def init_params(user_id: int, date1: str, date2: str) -> list:
+    param_list = []
+    params = {}
+
+    fields = ["ID", "TITLE", "STATUS", "CREATED_DATE", "CREATED_BY", "CLOSED_DATE", "DEADLINE", "DESCRIPTION"]
+
+    task_closed_params = Params(fields, params)
+    task_closed_params.add_select()
+    task_closed_params.add_filter('RESPONSIBLE_ID', user_id)
+    task_closed_params.add_filter('>REAL_STATUS', 4)
+    task_closed_params.add_filter('<=REAL_STATUS', 5)
+    task_closed_params.add_filter('>=CLOSED_DATE', f"{date1}")
+    task_closed_params.add_filter('<=CLOSED_DATE', f"{date2}")
+    task_closed_params.add_order('CREATED_DATE', 'asc')
+
+    param_list.append(task_closed_params)
+
+    task_in_work_params = Params(fields, params)
+    task_in_work_params.add_select()
+    task_in_work_params.add_filter('RESPONSIBLE_ID', user_id)
+    task_in_work_params.add_filter('<=REAL_STATUS', 4)
+    task_in_work_params.add_order('CREATED_DATE', 'asc')
+
+    param_list.append(task_in_work_params)
+
+    task_deferred_params = Params(fields, params)
+    task_deferred_params.add_select()
+    task_deferred_params.add_filter('RESPONSIBLE_ID', user_id)
+    task_deferred_params.add_filter('=REAL_STATUS', 6)
+    task_deferred_params.add_order('CREATED_DATE', 'asc')
+
+    param_list.append(task_deferred_params)
+
+    task_declined_params = Params(fields, params)
+    task_declined_params.add_select()
+    task_declined_params.add_filter('RESPONSIBLE_ID', user_id)
+    task_declined_params.add_filter('=REAL_STATUS', 7)
+    task_declined_params.add_order('CREATED_DATE', 'asc')
+
+    param_list.append(task_declined_params)
+
+    return param_list
+
+
+async def get_user_tasks(session, user_id, user, date1: str, date2: str):
+    param_list = init_params(user_id, date1, date2)
+    for par in param_list:
+        params = par.get_params()
+        await get_user_tasks_by_parameters(session, params, user)
+
+
+async def gather_tasks(user_list: list, date1: str, date2: str):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for current_user in user_list:
+            task = asyncio.create_task(
+                get_user_tasks(session, current_user.id, current_user, date1, date2))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
 
 
 def get_task_list(content: list, result: list) -> list:
